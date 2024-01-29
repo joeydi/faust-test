038dea9cfbec07b9597193148c0a7e2ec64e6107
Joe di Stefano <joeydi@okaypl.us>
2024-01-29 14:23:00 -0500

    update <Link>

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,40 +1,22 @@
-import Link from 'next/link';
-import { FeaturedImage } from '../FeaturedImage';
-import { PostInfo } from '../PostInfo';
-import styles from './Post.module.scss';
+import Link from "next/link";
+import { FeaturedImage } from "../FeaturedImage";
+import { PostInfo } from "../PostInfo";
+import styles from "./Post.module.scss";
 
-export default function Post({
-  title,
-  content,
-  date,
-  author,
-  uri,
-  featuredImage,
-}) {
-  return (
-    <article className={styles.component}>
-      {featuredImage && (
-        <Link href={uri}>
-          <a>
-            <FeaturedImage
-              image={featuredImage}
-              layout="responsive"
-              className={styles.featuredImage}
-            />
-          </a>
-        </Link>
-      )}
+export default function Post({ title, content, date, author, uri, featuredImage }) {
+    return (
+        <article className={styles.component}>
+            {featuredImage && (
+                <Link href={uri}>
+                    <FeaturedImage image={featuredImage} layout="responsive" className={styles.featuredImage} />
+                </Link>
+            )}
 
-      <Link href={uri}>
-        <a>
-          <h2>{title}</h2>
-        </a>
-      </Link>
-      <PostInfo date={date} author={author} className={styles.postInfo} />
-      <div
-        className={styles.content}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </article>
-  );
+            <Link href={uri}>
+                <h2>{title}</h2>
+            </Link>
+            <PostInfo date={date} author={author} className={styles.postInfo} />
+            <div className={styles.content} dangerouslySetInnerHTML={{ __html: content }} />
+        </article>
+    );
 }
